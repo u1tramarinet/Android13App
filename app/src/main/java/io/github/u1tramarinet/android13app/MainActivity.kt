3b2ad8e7f1435da963331c2728bbeae0d10dd925
Suzuki Takumi <u1tramarine.takumi@gmail.com>
2024-10-26 12:11:20 +0900

    Adjust for Android 13 and upgrade AGP.

--- app/src/main/java/io/github/u1tramarinet/android13app/MainActivity.kt
+++ app/src/main/java/io/github/u1tramarinet/android13app/MainActivity.kt
@@ -3,7 +3,6 @@ package io.github.u1tramarinet.android13app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -16,7 +15,6 @@ import io.github.u1tramarinet.android13app.ui.theme.Android13AppTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             Android13AppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->

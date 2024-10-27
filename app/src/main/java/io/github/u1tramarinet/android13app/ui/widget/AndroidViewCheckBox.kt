0d52f454b554dd0ba7058f38decadd27636099e0
Suzuki Takumi <u1tramarine.takumi@gmail.com>
2024-10-27 23:10:11 +0900

    No need for ThemedAndroidView.

--- app/src/main/java/io/github/u1tramarinet/android13app/ui/widget/AndroidViewCheckBox.kt
+++ app/src/main/java/io/github/u1tramarinet/android13app/ui/widget/AndroidViewCheckBox.kt
@@ -8,6 +8,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import io.github.u1tramarinet.android13app.ui.theme.AndroidViewInheritTheme
 
 @Composable
@@ -17,7 +18,7 @@ fun AndroidViewCheckBox(
     modifier: Modifier = Modifier,
     enabled: Boolean = true
 ) {
-    ThemedAndroidView(
+    AndroidView(
         modifier = modifier,
         factory = { context ->
             CheckBox(context).apply {
--- app/src/main/java/io/github/u1tramarinet/android13app/ui/widget/AndroidViewNumberPicker.kt
+++ app/src/main/java/io/github/u1tramarinet/android13app/ui/widget/AndroidViewNumberPicker.kt
@@ -7,12 +7,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
 import io.github.u1tramarinet.android13app.ui.theme.AndroidViewInheritTheme
 
 @Composable
 fun AndroidViewNumberPicker(modifier: Modifier = Modifier) {
     Row {
-        ThemedAndroidView(
+        AndroidView(
             modifier = modifier.background(color = Color.Black),
             factory = { context ->
                 NumberPicker(context)
